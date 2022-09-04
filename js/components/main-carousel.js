32efaae2e96e306b11e6182224e156cf67a21409
Danillo Silva <danillordm@gmail.com>
2022-09-04 10:08:38 -0300

    :sparkles: Add main carousel js

---
+++ js/components/main-carousel.js
@@ -0,0 +1,14 @@
+const btnRight = document.querySelector(".main-button.-arrow.-right");
+const btnLeft = document.querySelector(".main-button.-arrow.-left");
+const elements = document.querySelector(".elements");
+let count = 0;
+
+btnRight.addEventListener("click", function () {
+  count += 100;
+  elements.style = `transform: translateX(${count}px)`;
+});
+
+btnLeft.addEventListener("click", function () {
+  count -= 100;
+  elements.style = `transform: translateX(${count}px)`;
+});
