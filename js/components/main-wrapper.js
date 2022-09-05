bb09cb19ff2ccf09e074f1ee6b4a9bdd2c4c2d2d
Danillo Silva <danillordm@gmail.com>
2022-09-05 13:59:43 -0300

    :lipstick: Add text in skill cards

---
+++ js/components/main-wrapper.js
@@ -0,0 +1,34 @@
+//Skills Text
+const html = document.querySelector(".main-cards.-purple.-html");
+const css = document.querySelector(".main-cards.-purple.-css");
+const js = document.querySelector(".main-cards.-purple.-js");
+const react = document.querySelector(".main-cards.-purple.-react");
+let text2 = document.querySelector(".main-text.-second");
+
+html.addEventListener("mouseover", function changeText() {
+  text2.innerHTML =
+    "<span class= 'emphasis -second -title'>HTML</span> <br> É uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML.";
+});
+
+css.addEventListener("mouseover", function changeText() {
+  text2.innerHTML =
+    "<span class= 'emphasis -second -title'>CSS</span> <br> É uma linguagem para adicionar estilo (cores, fontes, espaçamento, etc.) a um documento web.";
+});
+
+js.addEventListener("mouseover", function changeText() {
+  text2.innerHTML =
+    "<span class= 'emphasis -second -title'>JavaScript</span> <br> é uma linguagem de programação interpretada estruturada, de script em alto nível com tipagem dinâmica fraca e multiparadigma (protótipos, orientado a objeto, imperativo e funcional).";
+});
+
+react.addEventListener("mouseover", function changeText() {
+  text2.innerHTML =
+    "<span class= 'emphasis -second -title'>React</span> <br> É uma biblioteca JavaScript de código aberto com foco em criar interfaces de usuário (front-end) em páginas web. É mantido pelo Facebook, Instagram, outras empresas e uma comunidade de desenvolvedores individuais. É utilizado nos sites da Netflix, Imgur, Feedly, Airbnb, SeatGeek, HelloSign, Walmart e outros.";
+});
+
+let quitCard = document.getElementsByClassName("quitCard");
+
+for (i = 0; i < quitCard.length; i++) {
+  quitCard[i].addEventListener("mouseout", function changeDefaultText() {
+    text2.innerHTML = "*passe o cursor no card para ler*";
+  });
+}
