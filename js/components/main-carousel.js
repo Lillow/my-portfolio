79dc1f225a864cab2c138215bc56e041cf1e5967
Danillo Silva <danillordm@gmail.com>
2022-09-05 14:01:55 -0300

    :lipstick: Fix text in carousel and skill cards

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,7 +1,9 @@
 // Move carousel
 const btnRight = document.querySelector(".main-button.-arrow.-right");
 const btnLeft = document.querySelector(".main-button.-arrow.-left");
-const elements = document.querySelector(".elements");
+const elements = document.querySelector(
+  ".main-wrapper.-grey.-second .elements"
+);
 let count = 0;
 
 btnRight.addEventListener("click", function () {
@@ -15,19 +17,24 @@ btnLeft.addEventListener("click", function () {
 });
 
 // Carousel text
-let text = document.querySelector(".main-text");
+let text1 = document.querySelector(".main-text");
 const generation = document.querySelector(".box.-generation");
+const estacio = document.querySelector(".box.-estacio");
 
-generation.addEventListener("mouseover", function () {
-  text.innerHTML =
-    "Em 2021 participei do bootcamp <span class= 'emphasis -generation'>Desenvolvedor Web Java Junior</span> na instituição <span class= 'emphasis -generation'>Generation Brasil</span> e lá aprendi práticas sobre: Metodologia Ágil - <span class= 'emphasis -generation'>SCRUM</span>, Lógica de Programação e Paradigma de Programação Orientada a Objetos em <span class= 'emphasis -generation'>Java</span>, versionamento (<span class= 'emphasis -generation'>Git</span>/<span class= 'emphasis -generation'>GitHub</span>), Banco de dados com <span class= 'emphasis -generation'>Mysql</span>, API Rest com <span class= 'emphasis -generation'>Spring Boot</span>, fundamentos web (<span class= 'emphasis -generation'>HTML5</span>, <span class= 'emphasis -generation'>CSS3</span>, <span class= 'emphasis -generation'>JavaScript</span> e <span class= 'emphasis -generation'>Bootstrap</span>), Experiência do Usuário - <span class= 'emphasis -generation'>UX</span>, componentização em <span class= 'emphasis -generation'>Angular</span>. Também aperfeiçoei e aprendi algumas habilidades e mentalidades como: Orientação ao Futuro, Responsabilidade Pessoal, Mentalidade de Crescimento, Persistência, Trabalho em Equipe, Atenção aos Detalhes, Proatividade e Comunicação. Lá pude descobrir o quão fascinante e estimulante é o mundo da <span class= 'emphasis -generation'>Programação</span>.";
+generation.addEventListener("mouseover", function changeText() {
+  text1.innerHTML =
+    "Em 2021 participou do bootcamp <span class= 'emphasis -generation'>Desenvolvedor Web Java Junior</span> na instituição <span class= 'emphasis -generation'>Generation Brasil</span> e lá aprendeu práticas sobre: Metodologia Ágil - <span class= 'emphasis -generation'>SCRUM</span>, Lógica de Programação e Paradigma de Programação Orientada a Objetos em <span class= 'emphasis -generation'>Java</span>, versionamento (<span class= 'emphasis -generation'>Git</span>/<span class= 'emphasis -generation'>GitHub</span>), Banco de dados com <span class= 'emphasis -generation'>Mysql</span>, API Rest com <span class= 'emphasis -generation'>Spring Boot</span>, fundamentos web (<span class= 'emphasis -generation'>HTML5</span>, <span class= 'emphasis -generation'>CSS3</span>, <span class= 'emphasis -generation'>JavaScript</span> e <span class= 'emphasis -generation'>Bootstrap</span>), Experiência do Usuário - <span class= 'emphasis -generation'>UX</span>, componentização em <span class= 'emphasis -generation'>Angular</span>. Também aperfeiçoou e aprendeu algumas habilidades e mentalidades como: Orientação ao Futuro, Responsabilidade Pessoal, Mentalidade de Crescimento, Persistência, Trabalho em Equipe, Atenção aos Detalhes, Proatividade e Comunicação. Lá pôde descobrir o quão fascinante e estimulante é o mundo da <span class= 'emphasis -generation'>Programação</span>.";
+});
+
+estacio.addEventListener("mouseover", function changeText() {
+  text1.innerHTML = "Estácio";
 });
 
 let quitBox = document.getElementsByClassName("quitBox");
 
 for (i = 0; i < quitBox.length; i++) {
   quitBox[i].addEventListener("mouseout", function changeDefaultText() {
-    text.innerHTML =
+    text1.innerHTML =
       "<span class='emphasis'>Gamer</span>, <span class='emphasis'>geek</span> e <span class='emphasis'>filósofo</span> nas horas vagas, entusiasta de <span class='emphasis'>ciência</span> e <span class='emphasis'>tecnologia</span>, apaixonado por desenvolvimento de <span class='emphasis'>software</span>, alguém que questiona quando necessário e que não tem medo de errar. Atualmente <span class='emphasis'>FullStack Developer Júnior</span> e estudante de <span class='emphasis'>Análise e Desenvolvimento de Sistemas</span>, <span class='emphasis'>Danillo</span> já trabalhou como auxiliar ADM e prestou serviços em eventos, anda trilhando seu caminho a procura de coisas novas e desafios, <span class='emphasis'>Danillo</span> também adora fazer amigos e quando não está <span class='emphasis'>codando</span> ou em busca de novos projetos, está jogando ou trocando ideia com amigos. <br /><br /><span class='emphasis -second'>*passe o cursor nas caixas para ver mais informações*</span>";
   });
 }
