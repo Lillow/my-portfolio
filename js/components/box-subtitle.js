9290d9ac6bb335a25998dd50aa4cb445765117dc
Danillo Silva <danillordm@gmail.com>
2022-09-07 12:52:12 -0300

    :lipstick: Add box subtitle component js

---
+++ js/components/box-subtitle.js
@@ -0,0 +1,14 @@
+const box = document.querySelector(".main-box.-grey");
+const subtitle = document.querySelector(".main-subtitle > .text2");
+
+function typeWrite(element) {
+  const textArray = element.innerHTML.split("");
+  element.innerHTML = " ";
+  textArray.forEach(function (letter, i) {
+    setTimeout(function () {
+      element.innerHTML += letter;
+    }, 100 * i);
+  });
+}
+
+typeWrite(subtitle);
