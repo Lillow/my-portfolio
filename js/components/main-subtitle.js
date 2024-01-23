dfdff7fc912896d75fb38f4406134fae21994396
Danillo Silva <33943534+Lillow@users.noreply.github.com>
2024-01-23 09:02:08 -0300

    Update main-subtitle.js

--- js/components/main-subtitle.js
+++ js/components/main-subtitle.js
@@ -2,14 +2,18 @@ const subtitle = document.querySelector(".main-subtitle > .text2");
 const box = document.querySelector(".main-box.-grey");
 
 function typeWrite(element) {
-  const textArray = element.innerHTML.split("");
-  element.innerHTML = " ";
-  textArray.forEach((letter, i) => {
-    setTimeout(() => (element.innerHTML += letter), 75 * i);
-  });
+	const textArray = element.innerHTML.split("");
+	element.innerHTML = " ";
+	textArray.forEach((letter, i) => {
+		setTimeout(() => (element.innerHTML += letter), 75 * i);
+	});
 }
 
 box.addEventListener("click", () => {
-  subtitle.innerHTML = "FullStack Developer";
-  typeWrite(subtitle);
+	if (subtitle == null) {
+		subtitle.innerHTML = "FullStack Developer";
+		typeWrite(subtitle);
+	} else {
+		subtitle = null;
+	}
 });
